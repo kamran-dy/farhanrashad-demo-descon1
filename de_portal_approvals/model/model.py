7be28e69846a001e807fd9a04c39294d33544335
dynexcel-ddc1 <ddc1@dynexce.co>
2021-08-20 12:49:41 +0500

    update

--- de_portal_approvals/model/model.py
+++ de_portal_approvals/model/model.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
-
+from datetime import date, timedelta
 
 
 class ResPartner(models.Model):
@@ -14,8 +14,32 @@ class ApprovalCategory(models.Model):
     
     
 class ApprovalRequest(models.Model):
-    _inherit = 'approval.request'   
+    _inherit = 'approval.request'
+
     
+    def  action_date_confirm_update(self):
+        for line in self:
+            date_confirm = fields.datetime.now() + timedelta(60)
+            line.update({
+               'date_confirmed': date_confirm
+            })
+            mail = self.env['mail.activity'].search([('res_id','=',line.id)])
+            date_dead = fields.date.today() + timedelta(60)
+            mail.update ({
+               'date_deadline': date_dead
+              })
+   
+    def  action_date_confirm_update_reverse(self):
+        for line in self:
+            date_confirm = fields.datetime.now() - timedelta(1)
+            line.update({
+               'date_confirmed': date_confirm
+            })
+            mail = self.env['mail.activity'].search([('res_id','=',line.id)])
+            date_dead = fields.date.today()  + timedelta(1)
+            mail.update ({
+               'date_deadline': date_dead
+              })     
 
 class ResUser(models.Model):
     _inherit = 'res.users'     
