7f6e8f0ffff2f168cf0500821206bce784fff511
dynexcel-ddc1 <ddc1@dynexce.co>
2021-09-08 08:54:24 +0500

    update addons

--- de_site_attendance/models/res_company.py
+++ de_site_attendance/models/res_company.py
@@ -9,20 +9,20 @@ class ResCompany(models.Model):
     
     from_date = fields.Integer('Period From')
     to_date = fields.Integer('Period To')
-    
+
     @api.onchange('from_date')
     def onchange_from_date(self):
         if self.from_date > 31:
             raise UserError('Not Allow To Enter grater than 31')
         if self.from_date < 0:
-            raise UserError('Not Allow To Enter grater than 0')    
-    
-    
+            raise UserError('Not Allow To Enter grater than 0')
+
+
     @api.onchange('to_date')
     def onchange_to_date(self):
         if self.to_date > 31:
             raise UserError('Not Allow To Enter grater than 31')
         if self.to_date < 0:
-            raise UserError('Not Allow To Enter grater than 0')    
-    
+            raise UserError('Not Allow To Enter grater than 0')
+
     
